3b15ba8abfa25086a2d73e703531e981f5e2cc97
wxyyxc1992 <384924552@qq.com>
2019-04-02 20:56:17 +0800

    chore: add export

--- fc-whiteboard/packages/fc-whiteboard/src/index.ts
+++ fc-whiteboard/packages/fc-whiteboard/src/index.ts
@@ -1,3 +1,4 @@
+export { EventHub } from './event/EventHub';
 export { Drawboard } from './board/Drawboard';
 export { Whiteboard } from './board/Whiteboard';
 export { WhiteboardMode } from './board/types';
